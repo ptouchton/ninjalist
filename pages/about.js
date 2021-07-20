a9f8331e353d99cff8d62140576d54eb33d4c349
Paul Touchton <paultouchton@gmail.com>
2021-07-20 11:49:12 -0700

    initial

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rerum officia sed. Eaque, corrupti quis ut repudiandae beatae voluptates ipsum veritatis cumque obcaecati temporibus libero, quisquam ratione, dolorum delectus quod.</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ pages/ninjas/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>Ninjas About</h1>
+            <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore qui maiores atque consequatur? Exercitationem expedita, consequuntur quos error porro nisi magnam incidunt distinctio quae labore ullam repudiandae provident amet neque?
+            </p>
+        </div>
+     );
+}
+ 
+export default About;
