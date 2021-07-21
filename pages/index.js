94e0860745c8e68584ca294bdca271b5481de878
Paul Touchton <paultouchton@gmail.com>
2021-07-21 16:10:50 -0700

    Layout created

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,18 @@ import Image from 'next/image'
 import Footer from '../comps/footer'
 import Navbar from '../comps/navbar'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nostrum eum, quidem natus iure dolorem cum sequi saepe necessitatibus ea laboriosam tenetur et voluptatibus dicta deleniti? Voluptate omnis consequuntur similique?
       </p>
-      <Footer />
+      <Link href='/ninjas'>
+        <a>See Ninja Listing</a>
+      </Link>
     </div>
   )
 }
