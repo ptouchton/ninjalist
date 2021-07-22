4f9fd3feae6b9e45f69d026810b9e748201f2b3f
Paul Touchton <paultouchton@gmail.com>
2021-07-21 20:22:47 -0700

    Head/Meta data

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,16 @@
+import Head from 'next/head'
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rerum officia sed. Eaque, corrupti quis ut repudiandae beatae voluptates ipsum veritatis cumque obcaecati temporibus libero, quisquam ratione, dolorum delectus quod.</p>
-        </div>
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="about" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae rerum officia sed. Eaque, corrupti quis ut repudiandae beatae voluptates ipsum veritatis cumque obcaecati temporibus libero, quisquam ratione, dolorum delectus quod.</p>
+            </div>
+        </>
     );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,20 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nostrum eum, quidem natus iure dolorem cum sequi saepe necessitatibus ea laboriosam tenetur et voluptatibus dicta deleniti? Voluptate omnis consequuntur similique?
-      </p>
-      <Link href='/ninjas'>
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nostrum eum, quidem natus iure dolorem cum sequi saepe necessitatibus ea laboriosam tenetur et voluptatibus dicta deleniti? Voluptate omnis consequuntur similique?
+        </p>
+        <Link href='/ninjas'>
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   )
 }
